5cfc681d70b7b88622b5baf1a6c90672bf9abc88
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-13 21:07:34 +0200

    Add files via upload

---
+++ Task05_Login/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task05_Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = Console.ReadLine();
+            string password = string.Empty;
+
+            for (int i = userName.Length - 1; i >= 0; i--)
+            {
+                password += userName[i];
+            }
+
+            
+            int counter = 0;
+            while (true)
+            {
+                string login = Console.ReadLine();
+                if (login == password)
+                    {
+                        Console.WriteLine($"User {userName} logged in.");
+                        break;
+                    }
+                else
+                    {
+                        counter += 1;
+                        if (counter == 4)
+                            {
+                                Console.WriteLine($"User {userName} blocked!");
+                                break;
+                            }
+                        Console.WriteLine("Incorrect password. Try again.");
+                }
+                
+            }            
+        }
+    }
+}
