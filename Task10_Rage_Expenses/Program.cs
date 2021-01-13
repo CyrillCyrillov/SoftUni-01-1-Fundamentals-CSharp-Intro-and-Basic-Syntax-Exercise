18aaabecdf425d728170eb6de25b4e1de99cb690
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-13 21:27:08 +0200

    Add files via upload

---
+++ Task10_Rage_Expenses/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task10_Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            float headsetPrice = float.Parse(Console.ReadLine());
+            float mousePrice = float.Parse(Console.ReadLine());
+            float keyboardtPrice = float.Parse(Console.ReadLine());
+            float displayPrice = float.Parse(Console.ReadLine());
+            
+            float cost = ((lostGames / 2) * headsetPrice) +
+                         ((lostGames / 3) * mousePrice) +
+                         ((lostGames / 6) * keyboardtPrice) +
+                         ((lostGames / 12) * displayPrice);
+
+            Console.WriteLine($"Rage expenses: {cost:F2} lv.");
+        }
+    }
+}
