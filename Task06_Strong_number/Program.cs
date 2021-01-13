311bdfb7a5cbb82b6e78fc2295eea6c5a9be81fa
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-13 21:10:09 +0200

    Add files via upload

---
+++ Task06_Strong_number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task06_Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int partOfNumber = number;
+            
+            while (true)
+            {
+                int nextDigit = partOfNumber % 10;
+                int nextDigitFactorial = 1;
+                for (int i = 1; i <= nextDigit; i++)
+                    {
+                        nextDigitFactorial = nextDigitFactorial * i;
+                    }
+                sum = sum + nextDigitFactorial;
+                partOfNumber = partOfNumber / 10;
+                if (partOfNumber == 0) break;
+            }
+            
+            if (sum == number)
+                {
+                    Console.WriteLine("yes");
+                }
+            else
+                {
+                    Console.WriteLine("no");
+                }            
+        }
+    }
+}
