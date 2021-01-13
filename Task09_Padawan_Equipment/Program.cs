0fffed42040cdc3c491262de9e72bcea45ac5d33
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-13 21:26:17 +0200

    Add files via upload

---
+++ Task09_Padawan_Equipment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task09_Padawan_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float amountOfMoney = float.Parse(Console.ReadLine());
+            int countOfStudents = int.Parse(Console.ReadLine());
+            float priceOfLightsabers = float.Parse(Console.ReadLine());
+            float priceOfRobes = float.Parse(Console.ReadLine());
+            float priceOfBelts = float.Parse(Console.ReadLine());
+
+            double neededMoney = (Math.Ceiling(countOfStudents * 1.1) * priceOfLightsabers) +
+                                 (countOfStudents * priceOfRobes) +
+                                 ((countOfStudents - (countOfStudents / 6)) * priceOfBelts);
+
+            if (amountOfMoney >= neededMoney)
+            {
+                Console.WriteLine($"The money is enough - it would cost {neededMoney:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {(neededMoney - amountOfMoney):f2}lv more.");
+            }
+        }
+    }
+}
