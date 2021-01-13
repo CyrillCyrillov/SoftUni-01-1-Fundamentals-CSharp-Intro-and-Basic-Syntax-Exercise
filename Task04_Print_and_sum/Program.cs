d05192c49900ce296af5707cde12760fd205fc4f
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-13 21:06:44 +0200

    Add files via upload

---
+++ Task04_Print_and_sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task04_Print_and_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endtNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = startNumber; i <= endtNumber; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
